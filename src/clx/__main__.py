286d73436c712412380e67efa6679fae689925e4
Matthias Hölzl <tc@xantira.com>
2024-08-06 01:02:39 +0200

    Initial commit

---
+++ src/clx/__main__.py
@@ -0,0 +1,18 @@
+
+import click
+
+
+def say_hi(name="world"):
+    click.echo(f"Hello, {name}!")
+
+
+@click.command()
+@click.option("--name", help="The name of the person to greet.", default="world")
+def main(name):
+    say_hi(name)
+
+
+if __name__ == "__main__":
+    main()
+
+
